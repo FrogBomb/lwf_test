5936890dd064d33d639071a06694a80821189564
FrogBomb <TLBlanchet@gmail.com>
2016-12-04 16:16:18 -0700

    Improved lwf_test -t
    Properly reload context for each test,  added more tests.

--- lwf_test/package_tests/_test.py
+++ lwf_test/package_tests/_test.py
@@ -7,6 +7,8 @@ Created on Thu Dec  1 16:27:59 2016
 @author: Tom Blanchet
 """
 from functools import wraps
+#import sys
+from importlib import reload
 
 def TestError(Exception):
     pass
@@ -16,6 +18,7 @@ def testDec(testFunc):
     global ALL_TESTS
     @wraps(testFunc)
     def testWrapper(*args, **kwargs):
+        _cleanTests()
         try:
             return True, testFunc(*args, **kwargs)
         except Exception as e:
@@ -23,6 +26,11 @@ def testDec(testFunc):
     ALL_TESTS.append(testWrapper)
     return testWrapper
 
+def _cleanTests():
+    import lwf_test
+    reload(lwf_test._lwf_test)
+    reload(lwf_test)
+    
 @testDec
 def importTest():
     import lwf_test
@@ -34,7 +42,7 @@ def importTest():
 def test0():
     
     from lwf_test import makeTester, printFinalResults,\
-                    SUCCESS_KEY, FAILURE_KEY, ERROR_KEY
+                    SUCCESS_KEY, FAILURE_KEY, ERROR_KEY, TestResultHelper
     
     @makeTester()
     def nothing():
@@ -79,7 +87,47 @@ def test0():
     assert objA.doSomething() == "1hi5"
     
     printFinalResults()
+    
+@testDec
+def testMakeTesterAlphaDoc():
+        from lwf_test import makeTester, TestResultHelper, disableVerboseTests
+        
+        disableVerboseTests() #Will not print individual outcomes this way
+        
+        @makeTester(catchErrors = ZeroDivisionError)
+        def myFunction(a, b):
+            return a/b
+        print(myFunction.Tester(5, 15, 3)) #Will return "success"
+        print(myFunction.Tester(1, 4, 2))  #Will return "failure"
+        print(myFunction.Tester(20, 4, 0)) #Will return "error"
+        try:
+            #This try block will raise an error that will not be caught
+            #and the test will not be registered in TestResultsHelper
+            
+            print(myFunction.Tester(3, "hi", "there"))
+            
+        except TypeError as te: 
+            print("fatal error:", te)
+            #raise
+            
+            #Ideally, you would want to raise these errors
+            #so that testing stops. 
+        
+        #Gets the TestResultsHelper instance for the named function. 
+        #Can also use TestResultsHelper.getInstanceForFunc(myFunction)
 
+        testHelper = TestResultHelper['myFunction']
+        
+        #Will print "3", ignoring the unexpected error
+        print("Total number of tests:", testHelper.getTotalTests())
+        
+        #Will print (in some order):
+        #>>> successes : 1
+        #>>> failures : 1
+        #>>> errors : 1
+        for k, v in testHelper.getOutcomeTotals().items():
+            print(k, ":", v)
+        
 @testDec
 def disableTest():
     from lwf_test import makeTester, printFinalResults, DISABLED_KEY,\
@@ -158,6 +206,7 @@ def main():
     for test in ALL_TESTS:
         print("Running", test.__name__)
         testRes = test()
+        
         try:
             if not testRes[0]:
                 raise testRes[1]
