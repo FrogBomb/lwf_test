63f2cc0e1b49b56c4f3423c9adbd1956d5773aa1
FrogBomb <TLBlanchet@gmail.com>
2016-12-04 16:28:58 -0700

    added lwf_test -r option
    -r : print README.md

--- lwf_test/__main__.py
+++ lwf_test/__main__.py
@@ -12,25 +12,35 @@ from codecs import open
 from os import path
 
 CONSOLE_HELP_FILE = "console_help.txt"
+README_FILE = "../README.txt"
 HERE = path.abspath(path.dirname(__file__))
 
 def argHandle(args):
+    
     ret = True
     if('-i' in args):
         print(lwf_test.__doc__)
+        ret = False
     elif('-h' in args):
         help(lwf_test)
+        ret = False
     elif('-t' in args):
         package_tests.run()
         ret = False
+    elif('-r' in args):
+        global README_FILE
+        printFile(README_FILE)
     return ret
     
+def printFile(file):
+    with open(path.join(HERE, file), encoding='utf-8') as f:
+            print(f.read())
+            
 def main(args = None):
     
     if(argHandle(args or sys.argv)):
         print(path.join(HERE, CONSOLE_HELP_FILE))
-        with open(path.join(HERE, CONSOLE_HELP_FILE), encoding='utf-8') as f:
-            print(f.read())
+        printFile(CONSOLE_HELP_FILE)
             
 if __name__ == "__main__":
     main()
